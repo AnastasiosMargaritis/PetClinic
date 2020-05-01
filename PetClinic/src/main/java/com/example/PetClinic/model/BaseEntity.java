f314fbb790518f2c568fb8a8736f9819dc1e341c
AnastasiosMargaritis <58393884+AnastasiosMargaritis@users.noreply.github.com>
2020-05-01 13:35:41 +0300

    POJO Models and Services
    Implementing POJO models and Base Entity and Services Interfaces with dummy methods.

---
+++ PetClinic/src/main/java/com/example/PetClinic/model/BaseEntity.java
@@ -0,0 +1,10 @@
+package com.example.PetClinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+
+}
---
+++ PetClinic/src/main/java/com/example/PetClinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.example.PetClinic.services;
+
+import com.example.PetClinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
