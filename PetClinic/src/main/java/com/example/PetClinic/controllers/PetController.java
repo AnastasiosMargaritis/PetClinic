ddaa25b7a127c9578ad385b60aba2a8a4ca6553c
AnastasiosMargaritis <58393884+AnastasiosMargaritis@users.noreply.github.com>
2020-05-28 23:14:52 +0300

    Update Pet Info.
    Implement PutMapping for Pet.

--- PetClinic/src/main/java/com/example/PetClinic/controllers/PetController.java
+++ PetClinic/src/main/java/com/example/PetClinic/controllers/PetController.java
@@ -2,6 +2,7 @@ package com.example.PetClinic.controllers;
 
 import com.example.PetClinic.model.Pet;
 import com.example.PetClinic.repositories.OwnerRepository;
+import com.example.PetClinic.repositories.PetRepository;
 import com.example.PetClinic.services.PetService;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +15,12 @@ import java.util.Set;
 public class PetController {
 
     private final PetService petService;
+    private final PetRepository petRepository;
     private final OwnerRepository ownerRepository;
 
-    public PetController(PetService petService, OwnerRepository ownerRepository) {
+    public PetController(PetService petService, PetRepository petRepository, OwnerRepository ownerRepository) {
         this.petService = petService;
+        this.petRepository = petRepository;
         this.ownerRepository = ownerRepository;
     }
 
@@ -31,12 +34,24 @@ public class PetController {
         return this.petService.findByOwnerId(id);
     }
 
-    @PostMapping("create/{id}")
-    public Optional<Pet> createPet(@PathVariable Long id, @RequestBody Pet pet){
+    @PostMapping("owners/{id}/create")
+    public Optional<Pet> createPet(@PathVariable Long id,
+                                   @RequestBody Pet pet){
 
         return ownerRepository.findById(id).map(owner -> {
             pet.setOwner(owner);
             return petService.save(pet);
+        });
+    }
+
+    @PutMapping("owners/{id}/details/{petId}")
+    public Optional<Pet> updatePet(@PathVariable Long ownerId,
+                                   @PathVariable Long petId,
+                                   @RequestBody Pet updatedPet){
+
+        return this.petRepository.findById(petId).map(pet -> {
+            pet.setName(updatedPet.getName());
+            return this.petRepository.save(pet);
         });
     }
 
